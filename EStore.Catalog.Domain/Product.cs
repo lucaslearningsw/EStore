653b9fe9451728a16c904e6c65fbeceda85596b6
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2023-09-13 12:02:51 -0300

    created base class entity

---
+++ EStore.Catalog.Domain/Product.cs
@@ -0,0 +1,19 @@
+﻿using EStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EStore.Catalog.Domain
+{
+    public class Product : Entity , IAggregateRoot
+    {
+
+    }
+
+    public class Category : Entity
+    {
+
+    }
+}
