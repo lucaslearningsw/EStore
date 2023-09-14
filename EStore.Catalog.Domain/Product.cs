d4de3a10b8dcab368b3f83c4b59f475fb4b912a0
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2023-09-14 10:25:38 -0300

    created domainException and AssertionConcern

--- EStore.Catalog.Domain/Product.cs
+++ EStore.Catalog.Domain/Product.cs
@@ -7,13 +7,83 @@ using System.Threading.Tasks;
 
 namespace EStore.Catalog.Domain
 {
-    public class Product : Entity , IAggregateRoot
+    public class Product : Entity, IAggregateRoot
     {
+        public Guid CategoryId { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool Active { get; private set; }
+        public decimal Price { get; private set; }
+        public DateTime DateTimeCreation { get; private set; }
+        public string Image { get; private set; }
+        public int QtyStock { get; private set; }
+        public Category Category { get; private set; }
 
+        public Product(string name, string description, bool active, decimal price, Guid categoryId, DateTime dateCreation, string image)
+        {
+            CategoryId = categoryId;
+            Name = name;
+            Description = description;
+            Active = active;
+            Price = price;
+            DateTimeCreation = dateCreation;
+            Image = image;
+        }
+
+        public void Activate() => Active = true;
+        public void Deactivate() => Active = false;
+
+
+        public void ChangeCategory(Category category)
+        {
+            Category = category;
+            CategoryId = category.Id;
+        }
+
+        public void ChangeDescription(string description)
+        {
+            Description = description;
+        }
+
+        public void DebitStock(int quantity)
+        {
+            if (quantity < 0) quantity *= -1;
+            QtyStock -= quantity;
+        }
+
+        public void ReplaceStock(int quantity)
+        {
+            QtyStock += quantity;
+        }
+
+        public bool HasStock(int quantity)
+        {
+            return QtyStock >= quantity;
+        }
+
+        public void Validate()
+        { 
+
+        }
+          
+        
     }
 
     public class Category : Entity
     {
+        public string Name { get; private set; }
+        public int Code { get; private set; }
+
+        public Category(string name, int code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Code}";
+        }
 
     }
 }
---
+++ EStore.Core/DomainObjects/Validations.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EStore.Core.DomainObjects
+{
+    public class Validations
+    {
+        public static void ValidateIfIsEqual(object object1, object object2, string mensage)
+        {
+            if (!object1.Equals(object2))
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateIfIsDifferent(object object1, object object2, string mensage)
+        {
+            if (object1.Equals(object2))
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateCaracteres(string value, int max, string mensage)
+        {
+            var lentgh = value.Trim().Length;
+            if (lentgh > max)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateCaracteres(string value, int min, int max, string mensage)
+        {
+            var lentgh = value.Trim().Length;
+            if (lentgh < min || lentgh > max)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateExpression(string pattern, string value, string mensage)
+        {
+            var regex = new Regex(pattern);
+
+            if (!regex.IsMatch(value))
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateIfIsNull(string value, string mensage)
+        {
+            if (value == null || value.Trim().Length == 0)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateIfIsNull(object object1, string mensage)
+        {
+            if (object1 == null)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateMinMax(double value, double min, double max, string mensage)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateMinMax(float value, float min, float max, string mensage)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateMinMax(int value, int min, int max, string mensage)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateMinMax(long value, long min, long max, string mensage)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateMinMax(decimal value, decimal min, decimal max, string mensage)
+        {
+            if (value < min || value > max)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateIfMinorEqualsMin(decimal value, decimal min, string mensage)
+        {
+            if (value <= min)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateIfMinorEqualsMin(int value, int min, string mensage)
+        {
+            if (value <= min)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateIfMinorEqualsMin(long value, long min, string mensage)
+        {
+            if (value <= min)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+
+        public static void ValidateIfMinorEqualsMin(double value, double min, string mensage)
+        {
+            if (value <= min)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+
+        public static void ValidateIfIsFalse(bool value, string mensage)
+        {
+            if (value)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+        public static void ValidateIfIsTrue(bool value, string mensage)
+        {
+            if (!value)
+            {
+                throw new DomainException(mensage);
+            }
+        }
+
+    }
+}
