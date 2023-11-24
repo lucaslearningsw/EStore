ead3b0b7a6358fe0c2e73d71e37ae1d8a43412e7
lucas.alixandre <lucas.alixandre@maisinteligencia.com.br>
2023-11-24 14:09:24 -0300

    fixed erros in views

--- src/EStore.Catalog.Application/Dtos/ProductDto.cs
+++ src/EStore.Catalog.Application/Dtos/ProductDto.cs
@@ -1,4 +1,6 @@
 ﻿
+using Microsoft.AspNetCore.Http;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 
@@ -9,40 +11,55 @@ namespace EStore.Catalog.Application.Dtos
         [Key]
         public Guid Id { get; set; }
 
+
+        [DisplayName("Nome")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Name { get;  set; }
 
+        [DisplayName("Descrição")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Description { get;  set; }
 
+
+        [DisplayName("Categoria")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public Guid CategoryId { get; set; }
 
+
+        [DisplayName("Ativo")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public bool Active { get;  set; }
 
+        [DisplayName("Preço")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Price { get;  set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public DateTime DateTimeCreation { get;  set; }
 
-        [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public string Image { get;  set; }
 
+        [DisplayName("Imagem do Produto")]
+        public IFormFile ImageUpload { get; set; }
+
+
+        [DisplayName("Quantidade em Estoque")]
         [Range(1,10000, ErrorMessage = "O campo {0} precisa ter o valor minimo de {1} e o máximo de {2}")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public int QtyStock { get;  set; }
 
 
+        [DisplayName("Altura")]
         [Range(1, int.MaxValue, ErrorMessage = "O campo {0} precisa ter o valor minimo de {1} e o máximo de {2}")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Height { get; set; }
 
+        [DisplayName("Largura")]
         [Range(1, int.MaxValue, ErrorMessage = "O campo {0} precisa ter o valor minimo de {1} e o máximo de {2}")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Width { get; set; }
 
+        [DisplayName("Profundidade")]
         [Range(1, int.MaxValue, ErrorMessage = "O campo {0} precisa ter o valor minimo de {1} e o máximo de {2}")]
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         public decimal Profundity { get; set; }
--- src/EStore.WebApp.MVC/Controllers/Admin/AdminProductController.cs
+++ src/EStore.WebApp.MVC/Controllers/Admin/AdminProductController.cs
@@ -32,7 +32,16 @@ namespace EStore.WebApp.MVC.Controllers.Admin
         public async Task<IActionResult> NewProduct(ProductDto productDto)
         {
 
-            if(!ModelState.IsValid) return View(await FillCategories(productDto));
+            ModelState.Remove("Image");
+            ModelState.Remove("ImageUpload");
+            if (!ModelState.IsValid) return View(await FillCategories(productDto));
+            var imgId = Guid.NewGuid() + "_";
+            if (!await UploadFile(productDto.ImageUpload, imgId))
+            {
+                return View(productDto);
+            }
+
+            productDto.Image = imgId + productDto.ImageUpload.FileName;
 
             await _productAppService.AddProduct(productDto);
 
@@ -52,13 +61,32 @@ namespace EStore.WebApp.MVC.Controllers.Admin
         [Route("update-product")]
         public async Task<IActionResult> UpdateProduct(Guid id, ProductDto productDto)
         {
-            var product = await _productAppService.GetById(id);
-            productDto.QtyStock = product.QtyStock;
 
+            if (id != productDto.Id) return NotFound(); 
+
+            var productUpdated = await _productAppService.GetById(id);
+
+            productDto.Image = productUpdated.Image;
+            productDto.QtyStock = productUpdated.QtyStock;
             ModelState.Remove("QtyStock");
-            if(!ModelState.IsValid) return View(await FillCategories(productDto));
 
-            await _productAppService.UpdateProduct(productDto);
+
+            if (!ModelState.IsValid) return View(await FillCategories(productDto));
+
+
+            if (productDto.ImageUpload != null)
+            {
+                var imgId = Guid.NewGuid() + "_";
+                if (!await UploadFile(productDto.ImageUpload, imgId))
+                {
+                    return View(productDto);
+                }
+
+                productUpdated.Image = imgId + productDto.ImageUpload.FileName;
+            }
+
+
+            await _productAppService.UpdateProduct(productUpdated);
             
             return RedirectToAction("Index");
         }
@@ -95,6 +123,28 @@ namespace EStore.WebApp.MVC.Controllers.Admin
         {
             productDto.Categories = await _productAppService.GetAllCategories();
             return productDto;
+        }
+
+
+
+        private async Task<bool> UploadFile(IFormFile imagemUpload, string imgId)
+        {
+            if (imagemUpload.Length <= 0) return false;
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/ImageS", imgId + imagemUpload.FileName);
+
+            if (System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError(string.Empty, "Já existe um arquivo com este nome!");
+                return false;
+            }
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await imagemUpload.CopyToAsync(stream);
+            }
+
+            return true;
         }
     }
 }
